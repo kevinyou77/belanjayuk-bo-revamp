091cdb5f5a887020f28977f3c4239db87fcb7899
Kevin Yulias <wb-kevinyuli532085@antfin.com>
2019-11-10 17:52:10 +0700

    imgur network helper separation

---
+++ src/constants/imgurApi.js
@@ -0,0 +1,2 @@
+export const IMGUR_BASE_URL = 'https://api.imgur.com/3'
+export const UPLOAD_IMAGE = `${this.BASE_URL}/upload`
--- src/network/ImgurNetworkHelper.js
+++ src/network/ImgurNetworkHelper.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import { 
+  IMGUR_BASE_URL,
+  UPLOAD_IMAGE
+} from './imgurApi'
 
 class ImgurEndpoints {
-  static BASE_URL = 'https://api.imgur.com/3'
-  static UPLOAD_IMAGE = `${this.BASE_URL}/upload`
-  
   static postImage ({ image, onSuccess, onError }) {
-    axios.post(this.UPLOAD_IMAGE, { image })
+    axios.post(UPLOAD_IMAGE, { image })
       .then(res => {
         onSuccess(res)
       })
