28cf50ced17ff2d802affdd8533aaf9010d568e3
Kevin Yulias <wb-kevinyuli532085@antfin.com>
2020-02-09 15:21:50 +0700

    add express page

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(port, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
